05f1287086d0474cf61c37c76d9c6962dc6ea471
Karen Holland <karen@brickfieldlabs.ie>
2025-06-16 16:24:23 +0100

    TW21351721 adding Irish translation

---
+++ lang/ga/local_bftranslate.php
@@ -0,0 +1,31 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Language strings for local_bftranslate.
+ *
+ * @package    local_bftranslate
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['apikey:azure'] = 'Eochair API Azure';
+$string['apikey:azure_desc'] = 'Cuir isteach d\'eochair API Azure chun aistriúcháin a chumasú.';
+$string['apikey:deepl'] = 'Eochair API DeepL';
+$string['apikey:deepl_desc'] = 'Cuir isteach d\'eochair API DeepL chun aistriúcháin a chumasú.';
+$string['branding:academy'] = 'Acadamh Oiliúna';
+
