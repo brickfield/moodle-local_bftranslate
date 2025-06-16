4cc9333663176e5996c0cf0c2f0b3fef21ac0722
Karen Holland <karen@brickfieldlabs.ie>
2025-06-16 15:33:57 +0100

    TW21351533 settings tidyup (#27)

--- settings.php
+++ settings.php
@@ -25,42 +25,44 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$ADMIN->add('localplugins', new admin_category('local_bftranslate_folder', get_string('pluginname', 'local_bftranslate')));
+if ($hassiteconfig) {
+    $ADMIN->add('localplugins', new admin_category('local_bftranslate_folder', get_string('pluginname', 'local_bftranslate')));
 
-$settings = new admin_settingpage('local_bftranslate', get_string('settings', 'local_bftranslate'));
+    $settings = new admin_settingpage('local_bftranslate', get_string('settings', 'local_bftranslate'));
 
-$settings->add(new admin_setting_configtext('local_bftranslate/deepl_api_key',
-    get_string('apikey:deepl', 'local_bftranslate'),
-    get_string('apikey:deepl_desc', 'local_bftranslate'),
-    '', PARAM_TEXT));
+    $settings->add(new admin_setting_configtext('local_bftranslate/deepl_api_key',
+        get_string('apikey:deepl', 'local_bftranslate'),
+        get_string('apikey:deepl_desc', 'local_bftranslate'),
+        '', PARAM_TEXT));
 
-$settings->add(new admin_setting_configtext('local_bftranslate/azure_api_key',
-    get_string('apikey:azure', 'local_bftranslate'),
-    get_string('apikey:azure_desc', 'local_bftranslate'),
-    '', PARAM_TEXT));
+    $settings->add(new admin_setting_configtext('local_bftranslate/azure_api_key',
+        get_string('apikey:azure', 'local_bftranslate'),
+        get_string('apikey:azure_desc', 'local_bftranslate'),
+        '', PARAM_TEXT));
 
-$settings->add(new admin_setting_configcheckbox(
-    'local_bftranslate/allowcoretranslation',
-    get_string('settings:allowcoretranslation', 'local_bftranslate'),
-    get_string('settings:allowcoretranslation_desc', 'local_bftranslate'),
-    '0'
-));
+    $settings->add(new admin_setting_configcheckbox(
+        'local_bftranslate/allowcoretranslation',
+        get_string('settings:allowcoretranslation', 'local_bftranslate'),
+        get_string('settings:allowcoretranslation_desc', 'local_bftranslate'),
+        '0'
+    ));
 
-$settings->add(new admin_setting_configcheckbox(
-    'local_bftranslate/showlocaltest',
-    get_string('settings:showlocaltest', 'local_bftranslate'),
-    get_string('settings:showlocaltest_desc', 'local_bftranslate'),
-    '0'
-));
+    $settings->add(new admin_setting_configcheckbox(
+        'local_bftranslate/showlocaltest',
+        get_string('settings:showlocaltest', 'local_bftranslate'),
+        get_string('settings:showlocaltest_desc', 'local_bftranslate'),
+        '0'
+    ));
 
-$settings->add(new admin_setting_configtextarea('local_bftranslate/external_plugins',
-    get_string('external_plugins', 'local_bftranslate'),
-    get_string('external_plugins_desc', 'local_bftranslate'),
-    '', PARAM_TEXT));
+    $settings->add(new admin_setting_configtextarea('local_bftranslate/external_plugins',
+        get_string('external_plugins', 'local_bftranslate'),
+        get_string('external_plugins_desc', 'local_bftranslate'),
+        '', PARAM_TEXT));
 
-$ADMIN->add('local_bftranslate_folder', $settings);
+    $ADMIN->add('local_bftranslate_folder', $settings);
 
-$ADMIN->add('local_bftranslate_folder', new admin_externalpage('local_bftranslate_report',
-        get_string('report:heading', 'local_bftranslate'),
-        new moodle_url('/local/bftranslate/index.php', []),
-        'moodle/site:config'));
+    $ADMIN->add('local_bftranslate_folder', new admin_externalpage('local_bftranslate_report',
+            get_string('report:heading', 'local_bftranslate'),
+            new moodle_url('/local/bftranslate/index.php', []),
+            'moodle/site:config'));
+}
